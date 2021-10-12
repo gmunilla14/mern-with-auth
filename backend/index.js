f6a213cc2e7e2665098cc510864c6392875b5c55
Greg Munilla <gregmunilla@gmail.com>
2021-10-12 19:53:16 -0400

    Create and Connect to MongoDB Atlas

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,31 @@
-const express = require('express')
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require('dotenv')
 
-const app = express()
+dotenv.config();
+
 
 //Create API endpoint
-app.get('/', (req, res) => {
-    res.send('Welcome to our todos api!!!')
-} )
+app.get("/", (req, res) => {
+  res.send("Welcome to our todos api!!!");
+});
 
-app.listen(5000, () => {
-    console.log('Server running on port 5000')
-})
+const connection_string = process.env.CONNECT_STRING
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+  console.log(`Server running on Port ${port}`);
+});
+
+mongoose
+  .connect(connection_string, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB connection established...");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
