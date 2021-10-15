00a9477091b4d51ec23c962792cca0a78bd5c5b8
Greg Munilla <gregmunilla@gmail.com>
2021-10-14 20:02:25 -0400

    Add Joi Data Validation

--- backend/routes/todos.js
+++ backend/routes/todos.js
@@ -1,10 +1,28 @@
 const { Todo } = require("../models/todo");
 const express = require("express");
+const Joi = require('joi')
 
 //Create instance of Router
 const router = express.Router();
 
 router.post("/", async (req, res) => {
+
+  //Add Joi validation
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(200).required(),
+    author: Joi.string().min(3).max(30),
+    uid: Joi.string(),
+    isComplete: Joi.boolean(),
+    date: Joi.date()
+  }).options( { abortEarly: false});
+
+  const { error} = schema.validate(req.body);
+ 
+  //If there is an error stops all the later code from happening and sends
+  //Error to client
+  if (error) return res.status(400).send(error.details[0].message)
+
+
   const { name, author, isComplete, date, uid } = req.body;
 
   let todo = new Todo({
