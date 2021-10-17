61338b0c2bab8aa4e8e7e4e42152c99ea3b92c3f
Greg Munilla <gregmunilla@gmail.com>
2021-10-17 17:19:42 -0400

    Create Nav Bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,25 @@
-import React from 'react';
-import {BrowserRouter, Route, Switch } from "react-router-dom"
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Container } from "@material-ui/core";
 
-import Todos from './components/todos/Todos'
-import SignIn from './components/auth/SignIn';
-import SignUp from './components/auth/SignUp';
-import NavBar from './components/navBar/NavBar';
+import Todos from "./components/todos/Todos";
+import SignIn from "./components/auth/SignIn";
+import SignUp from "./components/auth/SignUp";
+import NavBar from "./components/navBar/NavBar";
 
 function App() {
   return (
     <>
-        <BrowserRouter>
-          <NavBar/>
+      <BrowserRouter>
+        <Container maxWidth = "md">
+          <NavBar />
           <Switch>
-          <Route path="/signin" component = {SignIn}/>
-          <Route path="/signup" component = {SignUp}/>
-          <Route path="/" component = {Todos}/>
+            <Route path="/signin" component={SignIn} />
+            <Route path="/signup" component={SignUp} />
+            <Route path="/" component={Todos} />
           </Switch>
-
-        </BrowserRouter>
+        </Container>
+      </BrowserRouter>
     </>
   );
 }
--- frontend/src/components/navBar/NavBar.jsx
+++ frontend/src/components/navBar/NavBar.jsx
@@ -1,11 +1,56 @@
-import React from 'react';
+import React from "react";
+import { AppBar, Typography, Toolbar, Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { Link, useHistory } from "react-router-dom";
+
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+  },
+  linkStyle: {
+    color: "#fafafa",
+    textDecoration: "none",
+  },
+});
 
 const NavBar = () => {
-    return(
-        <>
-        <h2>NavBar</h2>
-        </>
-    );
-}
+  const classes = useStyles();
+  const history = useHistory();
+
+  const handleSignOut = () => {
+    //Sign out user
+    history.push("/signin");
+  };
+
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h4" className={classes.root}>
+            <Link to="/" className={classes.linkStyle}>
+              To Do App
+            </Link>
+          </Typography>
+          <Typography variant="subtitle2" className={classes.root}>
+            Logged in as ...
+          </Typography>
+          <Button color="inherit" onClick={() => handleSignOut()}>
+            SignOut
+          </Button>
+          <Button color="inherit">
+            <Link className={classes.linkStyle} to="/signin">
+              SignIn
+            </Link>
+          </Button>
+          <Button color="inherit">
+            <Link className={classes.linkStyle} to="/signup">
+              SignUp
+            </Link>
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
 
 export default NavBar;
